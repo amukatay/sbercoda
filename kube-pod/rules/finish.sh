d652623c6a6e153890ea71850ed5a46f8f1ef419
amukatay <72965617+amukatay@users.noreply.github.com>
2021-11-10 05:09:24 +0600

    Update finish.sh

--- kube-pod/rules/finish.sh
+++ kube-pod/rules/finish.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-objects=$(kubectl -n bookinfo get deployments,pods,replicasets,destinationrules,peerauthentications -o json 2>&1)
-
-# try without peerauthentications
-[ "$objects" = "No resources found" ] && objects=$(kubectl -n bookinfo get deployments,pods,replicasets,destinationrules -o json 2>&1)
-
-# give up
-[ "$objects" = "No resources found" ] && echo "$objects" && exit 1
+objects=$(/usr/local/bin/kubectl -n myapp get pods -o json 2>&1)
 
 echo $objects
