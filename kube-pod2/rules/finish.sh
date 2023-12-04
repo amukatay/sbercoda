cfe07c4d7575220af974751038c394b832cffd93
amukatay <72965617+amukatay@users.noreply.github.com>
2023-12-04 12:45:31 +0600

    Update finish.sh

--- kube-pod2/rules/finish.sh
+++ kube-pod2/rules/finish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-objects=$(/usr/local/bin/kubectl -n myapp get pods -o json 2>&1)
+#objects=$(/usr/local/bin/kubectl -n myapp get pods -o json 2>&1)
 #objects=$(/usr/local/bin/kubectl -n myapp get pods -o 2>&1)
 #sleep 180
 
-echo $objects
+#echo $objects
